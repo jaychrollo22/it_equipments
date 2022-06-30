b1fa1d769a4b1dce662361b1e28cc36ffbd1c50d
Arjay Lumagdong <arjay.lumagdong@lfuggoc.net>
2022-06-30 18:05:31 +0800

    add softdelete

--- app/Inventory.php
+++ app/Inventory.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use OwenIt\Auditing\Auditable;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Inventory extends Model implements AuditableContract
 {
     protected $connection = 'mysql';
     protected $guarded = [];
-
+    use SoftDeletes;
     use Auditable;
     protected $auditIncluded = [];
     protected $auditTimestamps = true;
